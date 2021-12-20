d9cd918cbcdea2d17050a1430ecc6b3cf2e84103
Alberto Acerbis <alberto.acerbis@4Solid.it>
2021-12-20 10:03:26 +0100

    Split a Blazor WASM application

---
+++ MultiProjectsRoute/Client/App.razor.cs
@@ -0,0 +1,77 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components.WebAssembly.Services;
+
+namespace MultiProjectsRoute.Client;
+
+public class AppBase : ComponentBase, IDisposable
+{
+    [Inject] private LazyAssemblyLoader AssemblyLoader { get; set; }
+    [Inject] private ILogger<App> Logger { get; set; }
+
+    protected readonly List<Assembly> LazyLoadedAssemblies = new();
+
+    protected async Task OnNavigateAsync(NavigationContext args)
+    {
+        try
+        {
+            switch (args.Path)
+            {
+                case "fetchdata":
+                    {
+                        var assemblies = await AssemblyLoader.LoadAssembliesAsync(new List<string>
+                        {
+                            "MultiProjectsRoute.Modules.WeatherForecast.dll"
+                        });
+                        LazyLoadedAssemblies.AddRange(assemblies);
+                        break;
+                    }
+
+                case "counter":
+                    {
+                        var assemblies = await AssemblyLoader.LoadAssembliesAsync(new List<string>
+                        {
+                            "MultiProjectsRoute.Modules.Counter.dll"
+                        });
+                        LazyLoadedAssemblies.AddRange(assemblies);
+                        break;
+                    }
+
+                // We need to load MultiProjects.Modules.Dashboard.dll at startup
+                default:
+                    {
+                        var assemblies = await AssemblyLoader.LoadAssembliesAsync(new List<string>
+                        {
+                            "MultiProjectsRoute.Modules.Dashboard.dll"
+                        });
+                        LazyLoadedAssemblies.AddRange(assemblies);
+                        break;
+                    }
+            }
+        }
+        catch (Exception ex)
+        {
+            Logger.LogError($"Error Loading spares page: {ex}");
+        }
+    }
+
+    #region Dispose
+    public void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+        }
+    }
+    public void Dispose()
+    {
+        this.Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    ~AppBase()
+    {
+        this.Dispose(false);
+    }
+    #endregion
+}
